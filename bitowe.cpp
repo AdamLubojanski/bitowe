d6eb2269b5647931c1405a0323be806606429f20
AdamLubojanski <62133278+AdamLubojanski@users.noreply.github.com>
2020-11-09 09:35:40 +0100

    Update bitowe.cpp

--- bitowe.cpp
+++ bitowe.cpp
@@ -13,12 +13,12 @@ int main() {
 
     cout<<"a= "<<bitset<8>(a).to_string()<<endl;
     cout<<"b= "<<bitset<8>(b).to_string()<<endl;
-    cout<<"a|b= "<<bitset<8>(a|b).to_string()<<endl;
-    cout<<"a&b= "<<bitset<8>(a&b).to_string()<<endl;
-    cout<<"a^b= "<<bitset<8>(a^b).to_string()<<endl;
+    cout<<"a|b= "<<bitset<8>(a^b).to_string()<<endl;
+    cout<<"a&b= "<<bitset<8>(a|b).to_string()<<endl;
+    cout<<"a^b= "<<bitset<8>(a&b).to_string()<<endl;
     cout<<"~a= "<<bitset<8>(~a).to_string()<<endl;
     cout<<"~b= "<<bitset<8>(~b).to_string()<<endl;
-    cout<<"a<<=4 "<<bitset<8>(a<<=4).to_string()<<endl;
+    cout<<"a<<=4 "<<bitset<8>(a<<=1).to_string()<<endl;
     cout<<"b>>=1 "<<bitset<8>(b>>=1).to_string()<<endl;
     return 0;
 }
