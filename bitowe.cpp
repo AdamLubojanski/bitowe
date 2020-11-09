17f54ed8203cc6bc3e4d436fef66b6d911a9d5c1
AdamLubojanski <62133278+AdamLubojanski@users.noreply.github.com>
2020-11-09 09:48:43 +0100

    Update bitowe.cpp

--- bitowe.cpp
+++ bitowe.cpp
@@ -11,14 +11,14 @@ int main() {
     cout<<"podaj b";
     cin>>b;
 
-    cout<<"a= "<<bitset<8>(a).to_string()<<endl;
-    cout<<"b= "<<bitset<8>(b).to_string()<<endl;
-    cout<<"a|b= "<<bitset<8>(a^b).to_string()<<endl;
-    cout<<"a&b= "<<bitset<8>(a|b).to_string()<<endl;
-    cout<<"a^b= "<<bitset<8>(a&b).to_string()<<endl;
-    cout<<"~a= "<<bitset<8>(~a).to_string()<<endl;
-    cout<<"~b= "<<bitset<8>(~b).to_string()<<endl;
-    cout<<"a<<=4 "<<bitset<8>(a<<=1).to_string()<<endl;
-    cout<<"b>>=1 "<<bitset<8>(b>>=1).to_string()<<endl;
+    cout<<"a= "<<bitset<8>(a)<<endl;
+    cout<<"b= "<<bitset<8>(b)<<endl;
+    cout<<"a|b= "<<(a^b)<<" "<<bitset<8>(a^b)<<endl;
+    cout<<"a&b= "<<(a|b)<<" "<<bitset<8>(a|b)<<endl;
+    cout<<"a^b= "<<(a&b)<<" "<<bitset<8>(a&b)<<endl;
+    cout<<"~a= "<<(~a)<<" "<<bitset<8>(~a)<<endl;
+    cout<<"~b= "<<(~b)<<" "<<bitset<8>(~b)<<endl;
+    cout<<"a<<=4 "<<(a<<=1)<<" "<<bitset<8>(a<<=1)<<endl;
+    cout<<"b>>=1 "<<(b>>=1)<<" "<<bitset<8>(b>>=1)<<endl;
     return 0;
 }
